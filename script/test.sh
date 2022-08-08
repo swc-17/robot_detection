001ee56e8c00826a221356dc1ce4727df1ce7557
swc-17 <2533884190@qq.com>
2022-08-08 11:18:44 +0800

    clean scripts

--- script/test.sh
+++ script/test.sh
@@ -1,22 +1,8 @@
-
-# distributed
-#     --launcher pytorch \
-# yolo psvoc
-
-
 export PYTHONPATH='.':$PYTHONPATH  
 mim test mmdet \
 	configs/parking_slot/yolov3_psvoc.py \
 	--gpus 4  \
-	--checkpoint work_dirs/yolo_1x_psvoc0712/latest.pth \
+	--checkpoint work_dirs/yolov3_psvoc/latest.pth \
 	--eval mAP \
 	--launcher pytorch
 	#--show-dir demo
-
-# export PYTHONPATH='.':$PYTHONPATH  
-# mim test mmdet \
-# 	configs/parking_slot/yolox_psvoc.py \
-# 	--gpus 1  \
-# 	--checkpoint work_dirs/yolox_psvoc/latest.pth \
-# 	--eval mAP
-# 	# --launcher pytorch \
--- script/train.sh
+++ script/train.sh
@@ -1,37 +1,4 @@
-# yolox ps
- PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-         configs/parking_slot/yolov3_psvoc.py \
-         --gpus 4  \
-         --launcher pytorch 
-
-# yolo ps configs/parking_slot/yolox_psvoc.py
-# PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#         configs/pascal_voc/yolo_1x_psvoc0712.py \
-#         --gpus 4  \
-#         --launcher pytorch 
-
-# yolov3 coco
-# PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#         configs/yolo/yolov3_d53_320_273e_coco.py \
-#         --gpus 4  \
-#         --launcher pytorch 
-
-# faster-rcnn voc
-# PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#         configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py \
-#         --gpus 4  \
-#         --launcher pytorch 
-
-# faster-rcnn ps
-#  PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#          configs/pascal_voc/faster_rcnn_r50_fpn_1x_psvoc0712.py \
-#          --gpus 4  \
-#          --launcher pytorch 
-
-# faster-rcnn voc single_gpu
-#PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#        configs/pascal_voc/faster_rcnn_r50_fpn_1x_voc0712.py 
-
-# faster-rcnn ps single gpu
-# PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
-#         configs/pascal_voc/faster_rcnn_r50_fpn_1x_psvoc0712.py 
+PYTHONPATH='.':$PYTHONPATH  mim train mmdet \
+        configs/parking_slot/yolov3_psvoc.py \
+        --gpus 4  \
+        --launcher pytorch 
